189380c6bd6d17aa6bc21d253f1eadf873572c35
Calvinno <42626008+Calvinno@users.noreply.github.com>
2019-01-02 15:58:14 -0500

    Add files via upload

---
+++ AppLifeCycle/AppLifeCycle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppLifeCycle
+//
+//  Created by Calvin Cantin on 18-10-03.
+//  Copyright © 2018 Calvin Cantin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
